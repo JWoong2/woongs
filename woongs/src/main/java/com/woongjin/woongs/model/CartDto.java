cb867047b64d9634bd2a9672819147577dcee546
dicky-1i1 <69330514+dicky-1i1@users.noreply.github.com>
2020-08-11 12:41:29 +0900

    th

---
+++ woongs/src/main/java/com/woongjin/woongs/model/CartDto.java
@@ -0,0 +1,22 @@
+package com.woongjin.woongs.model;
+
+public class CartDto {
+
+	private String user_id;
+	private int post_no;
+	
+	public String getUser_id() {
+		return user_id;
+	}
+	public void setUser_id(String user_id) {
+		this.user_id = user_id;
+	}
+	public int getPost_no() {
+		return post_no;
+	}
+	public void setPost_no(int post_no) {
+		this.post_no = post_no;
+	}
+	
+}
+
